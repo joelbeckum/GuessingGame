cb679b03e2b1f50d8ca14086b63fd70d6ceea4aa
Joel Beckum <joelbeckum@gmail.com>
2021-08-25 12:41:55 -0400

    Initial commit; Phase 1 of assignment completed

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess the Secret Number!");
+            Console.WriteLine("Type a number between 1 and 100 and hit enter:");
+
+            string response = Console.ReadLine();
+
+            Console.WriteLine($"You guessed {response}. Interesting choice...");
+        }
+    }
+}
